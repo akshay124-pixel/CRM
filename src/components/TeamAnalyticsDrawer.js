820220e02684f66e6f730e869ab5689b158e3811
Akshay Pratap Singh <akshay124-pixel@example.com>
2025-06-09 17:17:29 +0530

    Try

--- src/components/TeamAnalyticsDrawer.js
+++ src/components/TeamAnalyticsDrawer.js
@@ -156,7 +156,7 @@ const TeamAnalyticsDrawer = ({
     loading,
     retry,
   } = useCachedApi(
-    "https://crm-server-amz7.onrender.com/api/allusers",
+    "https://crm-server-amz7.onrender.com/api?users",
     localStorage.getItem("token")
   );
 
