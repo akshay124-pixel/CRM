60ced02d3ce5d2c93e25dce889a2798caa75fc73
Akshay Pratap Singh <akshay124-pixel@example.com>
2025-09-20 16:30:32 +0530

    Update Team Wise Anylitcs Ui

--- src/components/TeamAnalyticsDrawer.js
+++ src/components/TeamAnalyticsDrawer.js
@@ -954,12 +954,12 @@ const TeamAnalyticsDrawer = ({
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5 }}
-                style={{
-                  background: "rgba(255, 255, 255, 0.1)",
-                  borderRadius: "16px",
-                  padding: "20px",
-                  boxShadow: "0 8px 24px rgba(0, 0, 0, 0.3)",
-                }}
+                // style={{
+                //   background: "rgba(255, 255, 255, 0.1)",
+                //   borderRadius: "16px",
+                //   padding: "20px",
+                //   boxShadow: "0 8px 24px rgba(0, 0, 0, 0.3)",
+                // }}
               >
                 <Typography
                   sx={{
